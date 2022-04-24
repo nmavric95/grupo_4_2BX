2925e2ee4a130a1c3bbaad5b8410bd7f7d6a5d64
nmavric95 <mavricnadia@gmail.com>
2022-04-24 16:20:45 -0300

    Agregando index.html y su hoja de estilos, agregando el homePath

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express()
 const path = require("path")
 
 const publicPath = path.resolve(__dirname, "./public")
+const indexPath = path.resolve(__dirname, "./views/index.html")
 const productDetailPath = path.resolve(__dirname, "./views/productDetail.html")
 const port = 3030
 
 app.use(express.static(publicPath))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(productDetailPath))
+    res.sendFile(path.resolve(indexPath))
 })
 
 app.listen(port, () => console.log("Listening on port ", port))
