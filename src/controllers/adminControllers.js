42cf07af4dd6f04c402f4cc3dc1b7c1bf194b481
IvanAichino <ivanaichino@gmail.com>
2022-05-28 20:12:44 -0300

    Trabajo en edicion de un package (adminFormEdit)

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -6,6 +6,9 @@ const pathDB = path.resolve("./data/packageDB.json")
 const dataBasePackages = JSON.parse(fs.readFileSync(pathDB, "utf-8"))
 
 const adminControllers = {
+    adminBase : (req,res) =>{
+        res.render("./user/adminBase", {dataBasePackages: dataBasePackages})
+    },
     adminForm : (req, res) => {
         res.render("./user/adminForm")
     },
@@ -29,15 +32,43 @@ const adminControllers = {
 
         res.redirect("/userAdmin/adminBase");
     },
-    adminBase : (req,res) =>{
-        res.render("./user/adminBase", {dataBasePackages: dataBasePackages})
+    adminFormEdit : (req, res) => {
+        let id = req.params.idPackages
+        let packageToEdit = dataBasePackages.find(package => package.idPackages == id)
+        res.render("./user/adminFormEdit", {packageToEdit : packageToEdit} );
     },
-    adminDelete: (req, res) =>{
+    adminFormEditSend : (req, res) => {
+        let id = req.params.idPackages;
+        let packageToEdit = dataBasePackages.find(package => package.idPackages == id);
+
+        let image;
+        if(req.file[0] != undefined){
+            image = req.file[0].filename;
+        }else{
+            image = packageToEdit.image;
+        };
+
+        let packageEdited = {
+            idPackages : packageToEdit.idPackages,
+            ...req.body,
+            image : image,
+        };
+
+
+         let dataBasePackagesEdited = dataBasePackages.map(package => {
+             if(package.idPackages == id){
+                 return package = {...packageEdited}
+             }
+             return package;
+         })
+    }, 
+
+    adminDelete: (req, res) => {
         let idProduct = req.params.idPackages
         let dataBaseModified = dataBasePackages.filter(package => package.idPackages != idProduct)
         fs.writeFileSync(pathDB, JSON.stringify(dataBaseModified, null, " "))
         res.redirect("/userAdmin/adminBase");
-    }
-};
+    },
+}
 
 module.exports = adminControllers;
