f649e57ef88ecae3696c338d2655e4d562f00657
AnaCerruti09 <cerrutianamaria@gmail.com>
2022-06-05 14:27:36 -0300

    una parte de CRUD para vista perfil mas cambios en html y CSS

--- src/controllers/clientProfileController.js
+++ src/controllers/clientProfileController.js
@@ -1,7 +1,27 @@
+const path = require("path")
+const fs = require("fs")
+const pathDB = path.resolve("./data/userDB.json")
+
+const userDB = JSON.parse(fs.readFileSync(pathDB, "utf-8"))
+
+
 // PERFIL CLIENTE 
 const clientProfileController = {
-    clientProfile : (req, res) => {
-        res.render("./user/clientProfile")
+    clientProfile: (req, res) => {
+        res.render("./user/clientProfile"), ({userDB: userDB})
+    },
+
+    create: (req, res) =>{
+
+        let datos = {
+            edad: req.body.edad,
+            peso: req.body.peso,
+            altura: req.body.altura,
+            checkok: req.body.checkok,
+            tellus: req.body.tellus,
+        }
+
+       res.send(datos)
     },
 };
 
--- src/routes/cProfileRoute.js
+++ src/routes/cProfileRoute.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const multer = require("multer");
+
+//PARTE MULTER
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../public/img')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+const uploadFile = multer({ storage: storage })
+  
 
 const clientProfileController = require("../controllers/clientProfileController");
 
-//RUTA PERFIL CLIENTE/USUARIO
+
+//RUTA PERFIL USUARIO - ENVIAR INFORMACION
 router.get("/", clientProfileController.clientProfile);
+router.post("/", uploadFile.any(), clientProfileController.create);
+
+
+//RUTA PERFIL USUARIO - ACTUALIZAR DATOS
+
 
 module.exports = router;
