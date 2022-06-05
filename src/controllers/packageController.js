bc5b5b7fd4f7e9a8b2ec1fb0baa3dff272c3caf9
RodrigoAller <ro1989@gmail.com>
2022-06-05 20:20:20 -0300

    cambios en vista de paquetes para que traiga los paquetes de la base de datos

--- src/controllers/packageController.js
+++ src/controllers/packageController.js
@@ -6,9 +6,11 @@ const dataBasePackages = JSON.parse(fs.readFileSync(pathDB, "utf-8"))
 
 //CONTROLLER PARA PRODUCT DETAIL
 const packageController = {
-    packages : (req, res) => {
-        res.render("./packages/packages")
+    
+    packages : (req,res) =>{
+        res.render("./packages/packages", {dataBasePackages: dataBasePackages})
     },
+
     packagesDetail : (req, res) => {
         let id = req.params.idPackages;
         let packageToDetail = dataBasePackages.find(package => package.idPackages == id);
