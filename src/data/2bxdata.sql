4618ecb5f10afb00bc844aa4db296666ee7f7938
IvanAichino <ivanaichino@gmail.com>
2022-07-22 19:48:34 -0300

    Ingresando datos en base de datos desde workbench

--- src/data/2bxdata.sql
+++ src/data/2bxdata.sql
@@ -95,7 +95,7 @@ CREATE TABLE `locations` (
   `province` varchar(50) NOT NULL,
   `geoRegion` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,6 +104,7 @@ CREATE TABLE `locations` (
 
 LOCK TABLES `locations` WRITE;
 /*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'Buenos Aires','Centro'),(2,'Ciudad Autonoma de Buenos','Centro'),(3,'Catamarca','Norte'),(4,'Chaco','Norte'),(5,'Chubut','Sur'),(6,'Córdoba','Centro'),(7,'Corrientes','Norte'),(8,'Entre Ríos','Centro'),(9,'Formosa','Norte'),(10,'Jujuy','Norte'),(11,'La Pampa','Centro'),(12,'La Rioja','Norte'),(13,'Mendoza','Centro'),(14,'Misiones','Norte'),(15,'Neuquén','Sur'),(16,'Río Negro','Sur'),(17,'Salta','Norte'),(18,'San Juan','Centro'),(19,'San Luis','Centro'),(20,'Santa Cruz','Sur'),(21,'Santa Fe','Centro'),(22,'Santiago del Estero','Norte'),(23,'Tierra del Fuego','Sur'),(24,'Tucumán','Norte');
 /*!40000 ALTER TABLE `locations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +127,7 @@ CREATE TABLE `sports` (
   PRIMARY KEY (`id`),
   KEY `sportType_id` (`sportType_id`),
   CONSTRAINT `sportType_id` FOREIGN KEY (`sportType_id`) REFERENCES `sporttypes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,6 +136,7 @@ CREATE TABLE `sports` (
 
 LOCK TABLES `sports` WRITE;
 /*!40000 ALTER TABLE `sports` DISABLE KEYS */;
+INSERT INTO `sports` VALUES (1,'Parapente',4,1,16,3,0,1),(2,'Paracaidismo',4,1,18,4,1,0),(3,'Surf',1,0,10,1,0,1),(4,'Escalada',2,0,16,3,0,1),(5,'Rafting',1,1,10,2,1,1),(6,'Kitesurf',1,0,16,3,0,1),(7,'Trekking',2,0,10,1,0,1),(8,'Downhill',2,1,18,5,1,0),(9,'Buceo',1,1,18,2,1,0),(10,'Mountain Bike',2,0,10,2,0,1),(11,'Ski',3,1,10,3,1,1),(12,'Snowboard',3,1,10,3,1,1);
 /*!40000 ALTER TABLE `sports` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +151,7 @@ CREATE TABLE `sporttypes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,6 +160,7 @@ CREATE TABLE `sporttypes` (
 
 LOCK TABLES `sporttypes` WRITE;
 /*!40000 ALTER TABLE `sporttypes` DISABLE KEYS */;
+INSERT INTO `sporttypes` VALUES (1,'Agua'),(2,'Tierra'),(3,'Fuego'),(4,'Aire');
 /*!40000 ALTER TABLE `sporttypes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -202,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-21 22:02:53
+-- Dump completed on 2022-07-22 19:47:27
