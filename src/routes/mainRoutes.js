72763f247b8744898519cbb2760dd144df355946
IvanAichino <ivanaichino@gmail.com>
2022-08-08 22:36:58 -0300

    Trabajo sobre validaciones express validator para register

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -30,13 +30,22 @@ const logMiddleware = require('../middlewares/logMiddleware')
 // EXP-VALIDATOR REGISTER
 
 const registerValidation = [
-  body('name').notEmpty().withMessage('Completa tu nombre'),
-  body('lastName').notEmpty().withMessage('Completa tu apellido'),
+  body('name').notEmpty().withMessage('Completa tu nombre').bail()
+    .isLength({min : 2}).withMessage("Debes ingresar como mínimo 2 caracteres"),
+  body('lastName').notEmpty().withMessage('Completa tu apellido').bail()
+  .isLength({min : 2}).withMessage("Debes ingresar como mínimo 2 caracteres"),
   body('birthDate').notEmpty().withMessage('Completa tu fecha de nacimiento'),
   body('email')
   .notEmpty().withMessage('Completar tu email').bail()
   .isEmail().withMessage('Tenes que completar un email valido'),
-  body('password').notEmpty().withMessage('Inserta tu contraseña'),
+  body('password').notEmpty().withMessage('Inserta tu contraseña').bail()
+  .isLength({min : 8}).withMessage("La contraseña debe contener 8 caracteres mínimo"),
+  body('passwordRepeat').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('La contraseña no coincide');
+    } 
+    return true;
+  }),
 ];
 
 // VALIDATOR LOGIN
