05297a11c196aa1312a25430dceb67ce0b48fa1b
AnaCerruti09 <cerrutianamaria@gmail.com>
2022-08-20 23:19:58 -0300

    avanzando con el carro funcional, primeros cambios

--- src/controllers/productCartController.js
+++ src/controllers/productCartController.js
@@ -1,8 +1,30 @@
 //CONTROLLER PARA PRODUCT CART
+const path = require("path");
+const db = require("../database/models");
+const Activities = db.Activity;
+
+
 const productCartController = {
+
     productCart : (req, res) => {
         res.render("./productCart/productCart")
     },
+
+    productsSelected : (req, res) => {
+        let newProduct = [];
+        let package = req.params.idPackages;
+        Activities.findByPk(package, {
+            include : [
+            {association : "Location"},
+            {association : "Sport"}
+        ]
+    })
+
+        newProduct.push(package)
+        res.render("./productCart/productCart", {package})
+   
+    },
+
 };
 
 module.exports = productCartController;
--- src/routes/productCartRoutes.js
+++ src/routes/productCartRoutes.js
@@ -6,4 +6,7 @@ const productCartController = require("../controllers/productCartController");
 //RUTA PRODUCT CART
 router.get("/", productCartController.productCart);
 
+router.get("/:idPackages", productCartController.productsSelected);
+
+
 module.exports = router;
