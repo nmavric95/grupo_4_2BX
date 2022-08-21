e805a3119dfac0263138f3825ea57f44255a4e0e
IvanAichino <ivanaichino@gmail.com>
2022-08-21 19:00:47 -0300

    Trabajo en packages, cambio de estilos css a las atrjetas e inclusion de mas info en las mismas. Ademas se funcionalizo la barra de busqueda por filtros en la vista packages

--- src/controllers/packageController.js
+++ src/controllers/packageController.js
@@ -1,27 +1,32 @@
-const path = require("path");
-
 //BASE DE DATOS RELACIONAL
 const db = require("../database/models");
 const Activities = db.Activity;
+const Locations = db.Location;
+const Sports = db.Sport;
 
 //CONTROLLER PARA PRODUCT DETAIL
 const packageController = {
     
     packages : (req,res) =>{
-        Activities.findAll({
-            include : [
-                {association : "Location"},
-                {association : "Sport"}
-            ]
-        }).then(dataBasePackages => {
-            res.render("./packages/packages", {dataBasePackages: dataBasePackages})
-        })
+        let promActivities = Activities.findAll({
+                include : [
+                    {association : "Location"},
+                    {association : "Sport"}
+                ]
+            })
+        let promLocations = Locations.findAll();
+        let promSports =Sports.findAll();
+
+        Promise.all([promActivities, promLocations, promSports])
+            .then(([dataBasePackages, allLocations, allSports]) => {
+                res.render("./packages/packages", {dataBasePackages, allLocations, allSports})
+            })
+            .catch(error => res.send(error));
         
     },
 
     packagesDetail : (req, res) => {
         let id = req.params.idPackages;
-        // let packageToDetail = dataBasePackages.find(package => package.idPackages == id);
         Activities.findByPk(id, {
             include : [
                 {association : "Location"},
@@ -31,9 +36,96 @@ const packageController = {
             .then(packageToDetail => {
                 res.render("./packagesDetail/packagesDetail", {packageToDetail})
             })
-        
-        // res.render("./packagesDetail/packagesDetail", {packageToDetail : packageToDetail});
+            .catch(error => res.send(error));
     },
+
+    filter : (req, res) => {
+
+        let adrenaline = req.body.adrenalineLevel
+        let sport = req.body.sport
+        let location = req.body.location
+        let price = req.body.price
+        let sale = req.body.sale
+
+        let promActivities =  Activities.findAll({
+            include : [
+                {association : "Location"},
+                {association : "Sport"}
+            ]})
+
+        let promLocations = Locations.findAll();
+        let promSports =Sports.findAll();
+
+       
+        Promise.all([promActivities, promLocations, promSports])
+            .then(([dataBasePackagesUnfiltered, allLocations, allSports]) => {
+                
+                let dataBasePackages
+        
+                if(sport !== undefined){
+                    dataBasePackages = dataBasePackagesUnfiltered.filter(package => {
+                        return package.dataValues.sport_id == sport
+                   })
+                }
+
+                if(location !== undefined){
+                    if(dataBasePackages){
+                        dataBasePackages = dataBasePackages.filter(package => {
+                            return package.dataValues.location_id == location
+                       })
+                    }else{
+                        dataBasePackages = dataBasePackagesUnfiltered.filter(package => {
+                            return package.dataValues.location_id == location
+                       })
+                    }  
+                }
+
+                if(price !== ""){
+                    if(dataBasePackages){
+                        dataBasePackages = dataBasePackages.filter(package => {
+                            return package.dataValues.price <= price
+                       })
+                    }else{
+                        dataBasePackages = dataBasePackagesUnfiltered.filter(package => {
+                            return package.dataValues.price <= price
+                       })
+                    }  
+                }
+
+                if(sale !== undefined){
+                    if(dataBasePackages){
+                        dataBasePackages = dataBasePackages.filter(package => {
+                            return package.dataValues.sale_ratio != 0
+                       })
+                    }else{
+                        dataBasePackages = dataBasePackagesUnfiltered.filter(package => {
+                            return package.dataValues.sale_ratio != 0
+                       })
+                    }  
+                }
+
+                if(adrenaline !== undefined){
+                    if(dataBasePackages){
+                        dataBasePackages = dataBasePackages.filter(package => {
+                            return package.dataValues.Sport.dataValues.adrenaline_level == adrenaline
+                       })
+                    }else{
+                        dataBasePackages = dataBasePackagesUnfiltered.filter(package => {
+                            return package.dataValues.Sport.dataValues.adrenaline_level == adrenaline
+                       })
+                    }  
+                }
+
+                if(!dataBasePackages){
+                    dataBasePackages = dataBasePackagesUnfiltered
+                    return dataBasePackages
+                }
+                
+                res.render("./packages/packages", {dataBasePackages, allLocations, allSports})
+            })
+            .catch(error => res.send(error));
+        
+    }
 };
 
 module.exports = packageController;
--- src/routes/packageRoute.js
+++ src/routes/packageRoute.js
@@ -9,4 +9,6 @@ router.get("/", packageController.packages);
 //RUTA DE PACKAGE DETAIL
 router.get("/detail/:idPackages", packageController.packagesDetail);
 
+router.post("/search", packageController.filter);
+
 module.exports = router;
