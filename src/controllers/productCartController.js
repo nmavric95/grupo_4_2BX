0b8a1a9aa3a7ed93fc1150757971229c8ab0f284
IvanAichino <ivanaichino@gmail.com>
2022-08-21 21:40:05 -0300

    Continua trabajo sobre carrito de compras, envio de id por params ruta get

--- src/controllers/productCartController.js
+++ src/controllers/productCartController.js
@@ -6,12 +6,36 @@ const Activities = db.Activity;
 const Users = db.User;
 
 const productCartController = {
-
-    
-
     productCart : (req, res) => {
-        res.render("./productCart/productCart")
+        let toBuy = [];
+        res.render("./productCart/productCart", {toBuy})
+    },
+
+    productCartAdd : (req, res) => {
+        let id = req.params.idPackages;
+        let toBuy = [];
+
+        Activities.findByPk(id, {
+            include : [{association : "Location"}, {association : "Sport"}]
+        })
+        .then(packageToBuy => {
+            toBuy.push(packageToBuy)
+            res.render("./productCart/productCart", {toBuy})
+        })
+        .catch(error => res.send(error));
+
+
+         
+        // res.render("./productCart/productCart")
     },
 };
 
 module.exports = productCartController;
+
+
+// 
+// let package = req.params.idPackages;
+// 
+
+//     newProduct.push(package)
+//     
--- src/routes/productCartRoutes.js
+++ src/routes/productCartRoutes.js
@@ -6,4 +6,6 @@ const productCartController = require("../controllers/productCartController");
 //RUTA PRODUCT CART
 router.get("/", productCartController.productCart);
 
+router.get("/:idPackages", productCartController.productCartAdd);
+
 module.exports = router;
